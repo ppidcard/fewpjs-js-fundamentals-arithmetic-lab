22e72383fa6ceb664e01073fdb8e3857fa91de19
ppidcard <tvxqlikei@live.cn>
2021-03-29 21:01:17 +1100

    Done

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+
+const num1 = Math.trunc((Math.random()*100))+1;
+const num2 = 62/num1;
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+const random = Math.trunc(Math.random()) + 1;
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 20);
+
+/*
+
+  
+  describe('max', function() {
+    it("will return 20 as the highest number in the set", function() {
+      expect(max).to.eq(20)
+    });
+  });
+});
+*/
